e76cc27dd41cbd956482a3b8a87fa24003b6edc4
@NightlyLabs <119449604+nightlytech@users.noreply.github.com>
2022-12-29 18:13:16 +0300

    Update show.php

--- show.php
+++ show.php
@@ -1,7 +1,2 @@
-<?php
-function test1($long_name1,  $not_compress_local = ' ++') {
-    $long_name1 = strtolower($long_name1);
-    if(strlen($long_name1) > 10) {
-        return strtoupper($long_name1);
-echo $_SERVER['REMOTE_ADDR']
-?>
+<?php 
+ echo $_SERVER['REMOTE_ADDR'];
